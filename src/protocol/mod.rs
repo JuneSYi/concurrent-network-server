e8d1318753f9f34fd155b0e9d064bdf71240a7ac
junesyi <junetheyi@gmail.com>
2025-06-07 00:18:23 -0400

    refactor: protocol (separation of concerns)

---
+++ src/protocol/mod.rs
@@ -0,0 +1,16 @@
+// constants
+pub const HANDSHAKE:    u8 = b'*';
+pub const START_MARKER: u8 = b'^';
+pub const END_MARKER:   u8 = b'$';
+
+// returns true if this byte is the '^' start marker
+#[inline]
+pub fn is_start(b: u8) -> bool { b == START_MARKER }
+
+// returns true if this byte is the '$' end marker
+#[inline]
+pub fn is_end(b: u8) -> bool { b == END_MARKER }
+
+// transforms a byte according to this simple protocol implementation (add 1 mod 256)
+#[inline]
+pub fn transform(b: u8) -> u8 { b.wrapping_add(1) }
